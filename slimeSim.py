9b08bfe0e3b21b0c7834dcc8b30c0907639337eb
AHuss13 <Adamhuss13@gmail.com>
2024-12-09 12:23:27 -0600

    Added requirements.txt and initial slime simulation outline

---
+++ slimeSim.py
@@ -0,0 +1,17 @@
+
+
+# define the grid
+
+# define the slime agent
+
+# define the slime agent's movement
+
+# define the slime agent's interaction
+
+# define the slime agent's reproduction
+
+# define the slime agent's death
+
+# define the slime agent's environment
+
+# define the slime agent's food
